384993bba0267436e81c04af97ccecafa94b701e
flogtenb <36152895+flogtenb@users.noreply.github.com>
2019-12-02 17:27:38 +0100

    dishes-add screen: angular reactive-forms

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,24 +1,18 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { NavbarComponent } from "./navbar/navbar.component";
-import { HomeComponent } from "./home/home.component";
-import { IngredientsComponent } from "./ingredients/ingredients.component";
-import { DishesComponent } from "./dishes/dishes.component";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { IngredientsComponent } from './ingredients/ingredients.component';
+import { DishesComponent } from './dishes/dishes.component';
 import { DishesAddComponent } from './dishes-add/dishes-add.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    HomeComponent,
-    IngredientsComponent,
-    DishesComponent,
-    DishesAddComponent
-  ],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, NavbarComponent, HomeComponent, IngredientsComponent, DishesComponent, DishesAddComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- Angular/src/app/dishes-add/dishes-add.component.ts
+++ Angular/src/app/dishes-add/dishes-add.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+// import { FormControl } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-dishes-add',
@@ -6,10 +11,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dishes-add.component.css']
 })
 export class DishesAddComponent implements OnInit {
+  constructor() {}
+  name = new FormControl('');
+  dishForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    address: new FormGroup({
+      street: new FormControl(''),
+      city: new FormControl(''),
+      state: new FormControl(''),
+      zip: new FormControl('')
+    })
+  });
+  // profileForm = this.fb.group({
+  //   firstName: [''],
+  //   lastName: [''],
+  //   address: this.fb.group({
+  //     street: [''],
+  //     city: [''],
+  //     state: [''],
+  //     zip: ['']
+  //   })
+  // });
+  //  constructor(private fb: FormBuilder) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  // dishForm = new FormGroup({
+  //   firstName: new FormControl(''),
+  //   lastName: new FormControl('')
+  // });
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.dishForm.value);
   }
-
+  ngOnInit() {}
+  updateName() {
+    this.name.setValue('Nancy');
+  }
+  updateProfile() {
+    this.dishForm.patchValue({
+      firstName: 'Nancy',
+      address: {
+        street: '123 Drew Street'
+      }
+    });
+  }
 }
