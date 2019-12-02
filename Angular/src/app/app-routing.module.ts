df97213bae9f65e9325bbfcf4df51381c871b0db
flogtenb <36152895+flogtenb@users.noreply.github.com>
2019-12-02 15:05:44 +0100

    Angular: routing dishes-add

--- Angular/src/app/app-routing.module.ts
+++ Angular/src/app/app-routing.module.ts
@@ -5,18 +5,17 @@ import { HomeComponent } from './home/home.component';
 import { IngredientsComponent } from './ingredients/ingredients.component';
 import { HttpClientModule } from '@angular/common/http';
 import { DishesComponent } from './dishes/dishes.component';
+import { DishesAddComponent } from './dishes-add/dishes-add.component';
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent},
-  {path: 'ingredients', component: IngredientsComponent},
-  {path: 'dishes', component: DishesComponent},
+  { path: 'home', component: HomeComponent },
+  { path: 'ingredients', component: IngredientsComponent },
+  { path: 'dishes', component: DishesComponent },
+  { path: 'dishes-add', component: DishesAddComponent }
 ];
 
 @NgModule({
-  imports: [
-      RouterModule.forRoot(routes),
-      HttpClientModule
-  ],
+  imports: [RouterModule.forRoot(routes), HttpClientModule],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
