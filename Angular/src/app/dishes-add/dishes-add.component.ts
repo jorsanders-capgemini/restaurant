d27b73fa4b4e420011eade79dd263e77fb536010
flogtenb <36152895+flogtenb@users.noreply.github.com>
2019-12-09 12:28:38 +0100

    front-end: add-dish. Geen idee op dit moment nog hoe de ingevoerde data in het scherm toe te voeren aan de API (api/dish/save)

--- Angular/src/app/dishes-add/dishes-add.component.ts
+++ Angular/src/app/dishes-add/dishes-add.component.ts
@@ -11,25 +11,26 @@ import { FormArray } from '@angular/forms';
   styleUrls: ['./dishes-add.component.css']
 })
 export class DishesAddComponent implements OnInit {
-  // constructor() {}
-  // name = new FormControl('');
-  dishForm = new FormGroup({
-    description: new FormControl(''),
-    sellingPrice: new FormControl(''),
-    ingredients: new FormGroup({
-      ingredient1: new FormControl(''),
-      ingredient2: new FormControl(''),
-      ingredient3: new FormControl(''),
-      ingredient4: new FormControl(''),
-      ingredient5: new FormControl('')
-    }),
-    margin: new FormControl(''),
-    available: new FormControl('')
-  });
+  dishForm: FormGroup;
+  constructor() {}
+  ngOnInit() {
+    this.dishForm = new FormGroup({
+      description: new FormControl(''),
+      sellingPrice: new FormControl(''),
+      ingredients: new FormGroup({
+        ingredient1: new FormControl(''),
+        ingredient2: new FormControl(''),
+        ingredient3: new FormControl(''),
+        ingredient4: new FormControl(''),
+        ingredient5: new FormControl('')
+      }),
+      margin: new FormControl(''),
+      available: new FormControl('')
+    });
+  }
   onSubmit() {
     // TODO: Use EventEmitter with form value
     console.warn(this.dishForm.value);
     this.dishForm.reset();
   }
-  ngOnInit() {}
 }
